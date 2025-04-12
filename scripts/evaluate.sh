8debea61b693c2cc762e233127b0c5278747b709
haoyungweng <haoyungweng@gmail.com>
2025-04-12 16:02:10 -0400

    add data

--- scripts/evaluate.sh
+++ scripts/evaluate.sh
@@ -1,6 +1,6 @@
 python metric.py \
     --target_file data/dataset1/flan_test_200_selected_nstrict_1.jsonl \
     --target_key output \
-    --prediction_file outputs/fedavg-3B/20/19/client_0_output.jsonl \
+    --prediction_file outputs/hetlora-1B/20/19/client_0_output.jsonl \
     --prediction_key answer \
-    --output_dir results/fedavg-3B/20/ \
+    --output_dir results/hetlora-1B/20/ \
