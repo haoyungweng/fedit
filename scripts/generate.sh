aa7f1b1370c995bade7b70e1dd984a5c620bd0f3
haoyungweng <haoyungweng@gmail.com>
2025-04-12 03:29:59 -0400

    add 3B result

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -1,11 +1,11 @@
 cuda_devices=$1
 
 CUDA_VISIBLE_DEVICES="${cuda_devices}" python generate.py \
-    --base_model meta-llama/Llama-3.2-1B \
-    --output_dir /home/scratch/haoyungw/fedit/8/ \
+    --base_model meta-llama/Llama-3.2-3B \
+    --output_dir /home/scratch/haoyungw/fedit/fedavg-3B/8/ \
     --communication_rounds 19 \
     --prompt_template_name alpaca_short \
     --test_file_path "./data/dataset1/flan_test_200_selected_nstrict_1.jsonl" \
-    --save_dir ./outputs/fedavg-8/20/ \
-    --batch_size 2 \
+    --save_dir ./outputs/fedavg-3B/20/ \
+    --batch_size 4 \
     --fedavg
--- scripts/train.sh
+++ scripts/train.sh
@@ -1,10 +1,10 @@
 cuda_devices=$1
 
-CUDA_VISIBLE_DEVICES="${cuda_devices}" python main.py --global_model 'meta-llama/Llama-3.2-1B'\
+CUDA_VISIBLE_DEVICES="${cuda_devices}" python main.py --global_model 'meta-llama/Llama-3.2-3B'\
       --data_path  "./data/dataset1" \
       --val_data_path "./data/dataset1/flan_test_200_selected_nstrict_1.jsonl" \
-      --output_dir  '/home/scratch/haoyungw/fedit/'\
+      --output_dir  '/home/scratch/haoyungw/fedit/fedavg-3B/'\
       --num_communication_rounds 20 \
-      --num_clients  8 \
+      --num_clients 8 \
       --prompt_template_name 'alpaca_short' \
       --client_selection_frac 1
